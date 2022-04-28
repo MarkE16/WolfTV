dd28278b3ae5bfe0c56b53dd459224ccef08dc0b
Mark <markevola24@gmail.com>
2022-04-27 22:24:40 -0600

    added css changes

--- src/App.js
+++ src/App.js
@@ -4,24 +4,26 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className='App-header'>
-        <h1 className='App-title'>Welcome to WolfTV!</h1>
-        <ul>
+      <ul>
+        <header className='App-header'>
+          <img src={require("../src/assets/WolfTVText.png")} />
           <li><a href='#home'>Home</a></li>
           <li><a href='#about'>About</a></li>
           <li><a href='#more'>More</a></li>
-        </ul>
-      </header>
-      {/* <header className="App-header">
-        <h1 className='App-title'>Welcome to WolfTV!</h1>
-        <h3 className='App-title-items' onClick={() => {
-          console.log("Clicked Home")
-        }}>Home</h3>
-        <h3 className='App-title-items'>About</h3>
-        <h3 className='App-title-items'>More</h3>
-      </header> */}
-      <h1 className='Intro-title'>WOLF TV</h1>
-      <h2>hi</h2>
+          <input type="text" className='NavBar-input' placeholder='Find Something...'></input>
+        </header>
+      </ul>
+      <div id='home'>
+        <h1 className='Intro-title'>WOLF TV</h1>
+        <h1 className='Intro-title'>WOLF TV</h1>
+        <h1 className='Intro-title'>WOLF TV</h1>
+        <h1 className='Intro-title'>WOLF TV</h1>
+        <h1 className='Intro-title'>WOLF TV</h1>
+        <h1 className='Intro-title'>WOLF TV</h1>
+        <h1 className='Intro-title'>WOLF TV</h1>
+        <h1 className='Intro-title'>WOLF TV</h1>
+        <h2>hi</h2>
+      </div>
     </div>
   );
 }
